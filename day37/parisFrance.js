c92362748479c9e05c391c970b0aef295a79f140
Cory Bass <cory.bass.07@gmail.com>
2020-10-29 00:24:31 -0500

    parisFrance.js finished

--- day37/parisFrance.js
+++ day37/parisFrance.js
@@ -7,3 +7,24 @@
 //   .then((data) => {
 //     console.log(data);
 //   });
+const blvdsInParis = [
+  "Boulevard de l'Amiral-Bruix",
+  "Boulevard des Capucines",
+  "Boulevard de la Chapelle",
+  "Boulevard de Clichy",
+  "Boulevard de l'Hôpital",
+  "Boulevard des Italiens",
+  "Boulevard de la Madeleine",
+  "Boulevard de Magenta",
+  "Boulevard Marguerite-de-Rochechouart",
+  "Boulevard de Sébastopol",
+  "Boulevard de Strasbourg",
+  "Boulevard de la Zone",
+];
+
+const listMapper = () => {
+  return blvdsInParis.map((street) => {
+    pushToDom(".blvd-paris-target", "Li", street);
+  });
+};
+listMapper();
